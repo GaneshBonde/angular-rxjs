d0ee467b8857e87369a7ec173026c4a1e1eb683e
Ganesh Bonde <ganeshbonde@gmail.com>
2020-12-25 13:51:09 +0530

    AngularJs uinsg RxJs
    Created sample code for Angular and RxJs

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, VERSION , OnInit} from '@angular/core';
+import {of, from } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  implements OnInit{
+  name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    of(2,4,6,8).subscribe(console.log);
+    
+    from([20,15,10, 5]).subscribe(
+      item => console.log(`resulting item .. ${item}`),
+      err => console.error(`error occurred ... ${err}`),
+      () => console.log('Complete')
+
+    );
+
+    of('Apple1', 'Apple2' , 'Apple3')
+    .subscribe(
+      apple => console.log(`Apple was emmited ... ${apple}`),
+      err => console.error(`Error occured .. ${err}`),
+      () => console.log('No more apples, go home')
+    )
+  }
+}
