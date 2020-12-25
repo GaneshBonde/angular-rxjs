87b4f2d95a8ff0ab869c018c39db45cc7f613e3a
Ganesh Bonde <ganeshbonde@gmail.com>
2020-12-25 16:21:38 +0530

    Added operator's sample code
    Added operator's sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION , OnInit} from '@angular/core';
 import {of, from } from 'rxjs';
+import {map,tap, take} from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -12,7 +13,21 @@ export class AppComponent  implements OnInit{
   ngOnInit() {
     of(2,4,6,8).subscribe(console.log);
     
-    from([20,15,10, 5]).subscribe(
+    from([20,15,10, 5])
+    .pipe(
+      tap(item => console.log(`emitted item ... ${item}`)),
+      map(item => item * 2),
+      map(item => item -10),
+      map(item => {
+        if(item === 0){
+          throw new Error("zero detected ");
+        }
+
+        return item;
+      }),
+      take(3)
+    )
+    .subscribe(
       item => console.log(`resulting item .. ${item}`),
       err => console.error(`error occurred ... ${err}`),
       () => console.log('Complete')
